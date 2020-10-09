b3304aecea929eb2f7d1d57fc83325c64f7cfbb5
Adam H. James <ajames@technomics.net>
2020-10-09 17:30:48 -0400

    added readme and tweaked names

--- add_pkg.R
+++ add_pkg.R
@@ -1,10 +1,15 @@
 
+# Enter the name of the package you want to add to the repository
 pkg <-"readflexfile_0.2.0"
 
+# Location of the compiled file
 compile_loc <- file.path(setupr::get_dirs()$git_local, "costverse")
 
+# Insert the binary
 drat::insertPackage(paste0(compile_loc, "/", pkg, ".zip"), "docs")
+
+# Insert the source
 drat::insertPackage(paste0(compile_loc, "/", pkg, ".tar.gz"), "docs")
+
+# Cleanup
 drat::archivePackages("docs")
-
-
