f93c46657bfc3dfd59245cdf2cfd80c4256d27d8
Adam H. James <ajames@technomics.net>
2020-10-09 17:16:30 -0400

    renamed drat to docs for gh-pages

--- add_pkg.R
+++ add_pkg.R
@@ -3,8 +3,8 @@ pkg <-"readflexfile_0.2.0"
 
 compile_loc <- file.path(setupr::get_dirs()$git_local, "costverse")
 
-drat::insertPackage(paste0(compile_loc, "/", pkg, ".zip"), "drat")
-drat::insertPackage(paste0(compile_loc, "/", pkg, ".tar.gz"), "drat")
-drat::archivePackages("drat")
+drat::insertPackage(paste0(compile_loc, "/", pkg, ".zip"), "docs")
+drat::insertPackage(paste0(compile_loc, "/", pkg, ".tar.gz"), "docs")
+drat::archivePackages("docs")
 
 
