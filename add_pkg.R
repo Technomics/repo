17e376fb04e9d79b1cda0f3c62ce9c0b5e70655d
Adam H. James <ajames@technomics.net>
2020-10-16 18:00:02 -0400

    added 4.0 versions

--- add_pkg.R
+++ add_pkg.R
@@ -3,13 +3,14 @@
 pkg <-"costmisc_0.6.0"
 
 # Location of the compiled file
-compile_loc <- file.path(setupr::get_dirs()$git_local, "costverse")
+compile_loc <- file.path(setupr::get_dirs()$git_local, "costverse", "_builds")
 
 # Insert the binary
-drat::insertPackage(paste0(compile_loc, "/", pkg, ".zip"), "docs")
+drat::insertPackage(paste0(compile_loc, "/bin", "3.6", pkg, ".zip"), "docs")
+drat::insertPackage(paste0(compile_loc, "/bin", "4.0", pkg, ".zip"), "docs")
 
 # Insert the source
-drat::insertPackage(paste0(compile_loc, "/", pkg, ".tar.gz"), "docs")
+drat::insertPackage(paste0(compile_loc, "/src", pkg, ".tar.gz"), "docs")
 
 # Cleanup
 drat::archivePackages("docs")
